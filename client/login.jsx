2a6896819c77e3766c46b333300ced102b15f3d8
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-04-01 00:11:57 -0400

    login bundle created. Up to #27 Domo maker D

--- client/login.jsx
+++ client/login.jsx
@@ -51,3 +51,95 @@ const handleSignUp = (e) => {
     return false;
 
 };//end handle sign up clicks
+
+
+/**
+Since this is a simple component that will not update when the user
+types into it, we will make a “functional stateless component” or FSC.
+ */
+const LoginWindow = (props) => {
+    return(
+        <form id = 'loginForm'
+            name = 'loginForm'
+            onSubmit = {handleLogin}
+            action = '/login'
+            method = "POST"
+            className = 'mainForm'>
+
+        <label htmlFor='username'>Username: </label>
+        <input id='user' type= 'text' name='username' placeholder='username'/>
+        
+        <label htmlFor='password'>Password: </label>
+        <input id='pass' type='password' name='pass' placeholder='password' />
+
+        <input id='_csrf' type= 'hidden' name='_csrf' value={props.csrf} />
+
+        <input className='formSubmit' type='submit' value='Sign in' />
+
+        </form>
+    );
+};//loginwindow
+
+const SignupWindow = (props) => {
+    return(
+        <form id = 'signupForm'
+            name = 'signupForm'
+            onSubmit = {handleSignUp}
+            action = '/signup'
+            method = "POST"
+            className = 'mainForm'>
+
+        <label htmlFor='username'>Username: </label>
+        <input id='user' type= 'text' name='username' placeholder='username'/>
+        
+        <label htmlFor='pass'>Password: </label>
+        <input id='pass' type='password' name='pass' placeholder='password' />
+
+        <label htmlFor='pass2'>Password: </label>
+        <input id='pass2' type='password' name='pass2' placeholder='retype password' />
+
+        <input id='_csrf' type= 'hidden' name='_csrf' value={props.csrf} />
+
+        <input className='formSubmit' type='submit' value='Sign in' />
+        
+        </form>
+    );
+};//sign up window
+
+//initialize the page
+const init = async () => {
+    const response = await fetch('/getToken');
+    const data = await response.json();
+
+    //set up event listeners for when login/sign up is clicked
+    //when user presses button, 'content' section will be filled
+    const loginButton = document.getElementById('loginButton');
+    const signupButton = document.getElementById('signupButton');
+
+    loginButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        ReactDOM.render(
+            <LoginWindow csrf = {data.csrfToken} />,
+            document.getElementById('content')
+        );
+        return false;
+    });//login button event listener
+
+    signupButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        ReactDOM.render(
+            <SignupWindow csrf = {data.csrfToken} />,
+            document.getElementById('content')
+        );
+        return false;
+    });//signup button event listener
+
+    //want the user to be able to see something in the content section(ideally login)
+    ReactDOM.render(
+        <LoginWindow csrf = {data.csrfToken}/>,
+        document.getElementById('content')
+    );
+
+};//init
+
+window.onload = init;
